b36448968d4aae26f384ec5d3aa60019782b3871
Zulkifl Ajmal <zulkiflajmal@gmail.com>
2024-04-13 10:19:42 +0530

    Created hexa to decimal and working well!

--- numberSystemConverters.py
+++ numberSystemConverters.py
@@ -13,10 +13,29 @@ def decimal_to_hex(dec):
     print(hex_value)
 
 def hex_to_decimal(hexa):
-    pass
+    signs = []
+    for i in range(0,10):
+        signs.append(i)
+    signs = signs + ["A","B","C","D","E","F"]
+    values = list(reversed((" ".join(str(hexa))).split(" "))) # here we are adding a space between all letters
+    # values = values.split(" ") # spliting in the space
+    # values = list(reversed(values))
+    
+    k = 0
+    dec_value = 0
+    while len(values) > k:
+        n = 0
+        while n < 16:
+            if str(values[k]) == str(signs[n]):
+                dec_value += (16**k)*n
+            n += 1
+        k += 1
+
+    print(dec_value) 
 
 
 
 
 
-decimal_to_hex(23423742)
+# decimal_to_hex(23423742)
+hex_to_decimal("A2B6")
